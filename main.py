7dee7b0127924f842a60a26521fe344d64071a3a
Danysoftdev <danysoftdev@gmail.com>
2024-09-10 20:50:53 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""This module contains a function that calculates the sum of two numbers."""
+
+def suma(num1, num2):
+    """
+    Calculate the sum of two numbers.
+
+    Args:
+        num1 (int or float): The first number.
+        num2 (int or float): The second number.
+
+    Returns:
+        int or float: The sum of num1 and num2.
+    """
+    return num1 + num2
